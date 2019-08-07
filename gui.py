0b0ba0bc425a71e433b99ebc6900492c89449c6a
kszczedor <k.szczedor@gmail.com>
2019-08-07 23:10:52 +0200

    Change code formating

--- gui.py
+++ gui.py
@@ -6,10 +6,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
 
+
 class Main:
 
     def __init__(self, master):
-
         self.master = master
         self.color_fg = 'black'
         self.color_bg = 'white'
@@ -19,7 +19,7 @@ class Main:
         self.cnn_label = StringVar()
         self.dff_label = StringVar()
         self.digit = StringVar()
-        self.path = "_test.model"
+        self.path = ".model"
         self.pre_path = "models/"
 
         self.master.frame
@@ -32,12 +32,16 @@ class Main:
         self.c.bind('<Button-3>', self.clear)
         self.master.protocol("WM_DELETE_WINDOW", self.close)
 
-
         # load models or create new ones
         try:
-            self.cnn_model = tf.keras.models.load_model(self.pre_path+'cnn'+self.path)
+            self.cnn_model = tf.keras.models.load_model(
+                self.pre_path+'cnn' + self.path
+                )
         except:
-            if messagebox.askyesno("Load model error", "Do you want to create new CNN network model?"):
+            if messagebox.askyesno(
+                "Load model error",
+                "Do you want to create new CNN network model?"
+            ):
                 from network import Network
                 nn = Network()
                 nn.createCnnModel()
@@ -46,9 +50,13 @@ class Main:
                 self.master.destroy()
 
         try:
-            self.dff_model = tf.keras.models.load_model(self.pre_path+'dff'+self.path)
+            self.dff_model = tf.keras.models.load_model(
+                self.pre_path+'dff' + self.path)
         except:
-            if messagebox.askyesno("Load model error", "Do you want to create new DFF network model?"):
+            if messagebox.askyesno(
+                "Load model error",
+                "Do you want to create new DFF network model?"
+            ):
                 from network import Network
                 nn = Network()
                 nn.createDffModel()
@@ -56,39 +64,39 @@ class Main:
             else:
                 self.master.destroy()
 
-
     def paint(self, e):
-        
         if self.old_x and self.old_y:
-            self.c.create_line(self.old_x, self.old_y, e.x, e.y,
-                width=self.penwidth, fill=self.color_fg, 
-                capstyle=ROUND, smooth=True)
+            self.c.create_line(
+                self.old_x, self.old_y, e.x, e.y,
+                width=self.penwidth, fill=self.color_fg,
+                capstyle=ROUND, smooth=True
+            )
 
         self.old_x = e.x
         self.old_y = e.y
 
     def paintDot(self, e):
+        self.c.create_oval(
+            e.x - self.penwidth/2, e.y - self.penwidth/2,
+            e.x + self.penwidth/2, e.y + self.penwidth/2, fill=self.color_fg
+        )
 
-        self.c.create_oval(e.x - self.penwidth/2, e.y - self.penwidth/2, 
-            e.x + self.penwidth/2, e.y + self.penwidth/2, fill=self.color_fg)
-
-    def reset(self, e):
-       
+    def reset(self, e):    
         self.old_x = None
         self.old_y = None
 
     def clear(self, e=None):
-        
         self.c.delete(ALL)
 
     def drawWidgets(self):
-        
         # canvas
         self.c = Canvas(self.master, width=280, height=280,  bg=self.color_bg)
-        self.c.grid(row=0, column=0, columnspan=28, rowspan=28,sticky=W+E+N+S, padx=5, pady=5)
+        self.c.grid(row=0, column=0, columnspan=28, rowspan=28,
+                    sticky=W+E+N+S, padx=5, pady=5)
 
         # prediction labels
-        Label(self.master, text="Prediction").grid(row=0, column=29 ,sticky=W, columnspan=30)
+        Label(self.master, text="Prediction").grid(row=0, column=29, 
+                                                   sticky=W, columnspan=30)
 
         Label(self.master, text="CNN:").grid(row=1, column=29 ,sticky=W)
         Label(self.master, text="0", width=3, textvariable=self.cnn_label).grid(row=1, column=30 ,sticky=W)
@@ -106,7 +114,6 @@ class Main:
         Button(self.master, text="Learn", command=self.learn).grid(row=21, column=29, columnspan=30)
 
     def prepareData(self):
-
         # image coords
         x = self.master.winfo_rootx() + self.c.winfo_x()
         y = self.master.winfo_rooty() + self.c.winfo_y()
@@ -114,7 +121,9 @@ class Main:
         y1 = y + self.c.winfo_height()
 
         # resize to 28x28, anti-aliasing, invertic
-        img = PIL.ImageGrab.grab().crop((x,y,x1,y1)).resize((28, 28), resample=Image.LANCZOS).convert('L')
+        img = PIL.ImageGrab.grab()
+        img = img.crop((x, y, x1, y1)).resize((28, 28), resample=Image.LANCZOS)
+        img = img.convert('L')
         img = PIL.ImageOps.invert(img)
 
         # img -> np.array, normalize
@@ -122,34 +131,34 @@ class Main:
         arr_n = tf.keras.utils.normalize(arr, axis=0)
 
         # transform np.array to each model shape
-        arr_cnn = np.array([np.expand_dims(arr_n,-1)])
+        arr_cnn = np.array([np.expand_dims(arr_n, -1)])
         arr_dff = np.array([[arr_n]])
         return arr_cnn, arr_dff
 
-
     def predict(self, e=None):
-
         arr_cnn, arr_dff = self.prepareData()
 
         # predict
-        prediction = {'cnn':self.cnn_model.predict(arr_cnn), 'dff':self.dff_model.predict(arr_dff)}
+        prediction = {'cnn': self.cnn_model.predict(arr_cnn),
+                      'dff': self.dff_model.predict(arr_dff)}
 
         # update prediction labels
         self.cnn_label.set(str(np.argmax(prediction['cnn'])))
         self.dff_label.set(str(np.argmax(prediction['dff'])))
 
-        
     def learn(self):
 
-        y =  np.array([int(self.digit.get()[0])])
+        y = np.array([int(self.digit.get()[0])])
         x_cnn, x_bp = self.prepareData()
 
         self.cnn_model.train_on_batch(x_cnn[0:1], y)
         self.dff_model.train_on_batch(x_bp[0:1], y)
 
     def close(self):
-        if messagebox.askyesno("Quit", "Do you want to save training progress before quit?"):
-            
+        if messagebox.askyesno(
+            "Quit",
+            "Do you want to save training progress before quit?"
+        ):
             self.cnn_model.save(self.pre_path+'cnn'+self.path)
             self.dff_model.save(self.pre_path+'dff'+self.path)
             self.master.destroy()
